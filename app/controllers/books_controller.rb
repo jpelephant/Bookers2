79a7a9907c9f7718e6dfedd81689910568601193
jpelephant <yuto.20020410@gmail.com>
2021-09-02 12:52:37 +0000

    [Add]4-10

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to post_images_path
+  end
+
+  def index
+    @book = Book.all
+  end
+
+  def show
+    @book = Book.find(params[id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @boook.destroy
+    redirect_to book_path
+  end
+
+   private
+
+  def book_params
+    params.require(:book).permit(:shop_name, :image, :caption)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  root to: 'homes#top'
   devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users,only: [:show, :edit, :update]
 end
